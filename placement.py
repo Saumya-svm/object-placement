6d7e2d6e188715df700db88dbd1076283fbfe7b4
Saumya Mundra <saumyamundra@gmail.com>
2022-12-16 00:12:41 +0530

    edited placement.py

--- placement.py
+++ placement.py
@@ -10,19 +10,25 @@ from compose import compose_images
 from loader import dataset_dict, get_loader
 from loader.utils import gen_composite_image
 
-
-def sample(sample_dataset, model, iter, gen_dir):
-    model.start_eval()
-    id_list = [0, 1500, 2500, 3500, 4500, 5500, 6500, 7500, 8500, 10000]
-    for id in id_list:
-        index, bg_feat, fg_feat, fg_msk_feat, fg_bbox, comp_feat, comp_msk_feat, comp_crop_feat, label, trans_label, catnm = sample_dataset[id]
-        pred_img, pred_msk, pred_trans = model.test_genorator(bg_feat.unsqueeze(0), fg_feat.unsqueeze(0), fg_msk_feat.unsqueeze(0), torch.Tensor(fg_bbox).unsqueeze(0))
-        img = transforms.ToPILImage()(pred_img.cpu()[0]).convert('RGB')
-        sample_dir = os.path.join(gen_dir, str(id))
-        if not os.path.exists(sample_dir):
-            os.makedirs(sample_dir)
-        img.save(os.path.join(sample_dir, '{}.jpg'.format(iter)))
-
+class args:
+    def __init__(self):
+        self.lr = 0.00002
+        self.d_noise = 1024
+        self.d_model = 512
+        self.d_k = 64
+        self.d_v = 64
+        self.n_heads = 8
+        self.len_k = 84
+        self.b1 = 0.5
+        self.b2 = 0.999
+        self.dst = "OPADst1"
+        self.img_size = 256
+        self.expid = 'graconet'
+        self.data_root = "new_OPA"
+        self.eval_type = "eval"
+        self.epoch = 11
+        self.repeat = 1
+        self.category = 'person'
 
 def infer(eval_loader, opt, model=None, repeat=1):
     def csv_title():
@@ -94,35 +100,31 @@ def info(comp, mask, bbox, scale):
     os.rename(composite_path, 'new_OPA/'+new_img_path)
     mask.save(mask_path)
     os.rename(mask_path, 'new_OPA/'+new_msk_path)
+    # annID = '3'
+    # scID = '4'
+    # bbox = '[216, 100, 363, 546]'
+    # scale = 0.99
+    # label = '1'
+    # catnm = 'dog'
+    # new_img_path = 'composite/test_set/3_4_71_338_101_154_0.99_1.jpg'
+    # new_msk_path = 'composite/test_set/mask_3_4_71_338_101_154_0.99_1.jpg'
+    # annID = '3'
+    # scID = '4'
+    # bbox = '[71, 338, 101, 154]'
+    # scale = 0.0001
+    # label = '1'
+    # catnm = 'dog'
+    # new_img_path = 'composite/test_set/3_4_71_338_101_154_0.1_0.jpg'
+    # new_msk_path = 'composite/test_set/mask_3_4_71_338_101_154_0.1_0.jpg'
     info = [0, int(annID), int(scID),
             bbox, scale, int(label), catnm,
             new_img_path, new_msk_path]
     return [info]
     
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--lr", type=float, default=0.00002, help="adam: learning rate")
-    parser.add_argument("--d_noise", type=int, default=1024, help="dimension of random noise/vector")
-    parser.add_argument("--d_model", type=int, default=512, help="dimension of features")
-    parser.add_argument("--d_k", type=int, default=64, help="dimension of key in multi-head attention")
-    parser.add_argument("--d_v", type=int, default=64, help="dimension of value in multi-head attention")
-    parser.add_argument("--n_heads", type=int, default=8, help="number of heads in multi-head attention")
-    parser.add_argument("--len_k", type=int, default=84, help="number of background nodes")
-    parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-    parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
-    parser.add_argument("--dst", type=str, choices=list(dataset_dict.keys()), default="OPADst1", help="dataloder type")
-    parser.add_argument("--img_size", type=int, default=256, help="size of image", required=False)
-    parser.add_argument("--expid", type=str, default='graconet', help="experiment name", required=False)
-    parser.add_argument("--data_root", type=str, default="new_OPA", help="dataset root")
-    parser.add_argument("--eval_type", type=str, choices=["train", "trainpos", "sample", "eval", "evaluni", "eval_self"], default="eval", help="evaluation type")
-    parser.add_argument("--epoch", type=int, default=11, help="which epoch to evaluate", required=False)
-    parser.add_argument("--repeat", type=int, default=1, help="number of times to sample different random vectors")
-    parser.add_argument("--category", type=str, default='person', help="category of the foreground")
-    opt = parser.parse_args()
-
-    scale = input()
+def place(scale=0.99):
+    opt = args()
+    scale = scale
     category = opt.category
-    print(category)
     # path to transparent foreground
     foreground = f'new_OPA/transparent_mask/{category}/1.png'
     # path to background
@@ -131,7 +133,10 @@ if __name__ == '__main__':
     comp, mask, bbox = compose_images(foreground, background)
     # path to mask for foreground
     foreground_mask = f'new_OPA/transparent_mask/{category}/mask_1.png'
+    # if not os.path.exists(f'new_OPA/foreground/{category}'):
+    #     os.mkdir(f'new_OPA/foreground/{category}')
     category_dir = f'new_OPA/foreground/{category}/'
+    # shutil.copy(background, f'new_OPA/background/{category}/2.jpg')
     shutil.copy(foreground, category_dir+'1.jpg')
     shutil.copy(foreground_mask, category_dir+'mask_1.jpg')
     os.mkdir('new_OPA/composite/')
