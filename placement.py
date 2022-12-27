1318ff3126951878ee102788de44feb89260733d
Saumya Mundra <81912330+Saumya-svm@users.noreply.github.com>
2022-12-27 22:39:45 +0530

    Added placement

--- placement.py
+++ placement.py
@@ -10,25 +10,32 @@ from compose import compose_images
 from loader import dataset_dict, get_loader
 from loader.utils import gen_composite_image
 
-class args:
-    def __init__(self):
-        self.lr = 0.00002
-        self.d_noise = 1024
-        self.d_model = 512
-        self.d_k = 64
-        self.d_v = 64
-        self.n_heads = 8
-        self.len_k = 84
-        self.b1 = 0.5
-        self.b2 = 0.999
-        self.dst = "OPADst1"
-        self.img_size = 256
-        self.expid = 'graconet'
-        self.data_root = "new_OPA"
-        self.eval_type = "eval"
-        self.epoch = 11
-        self.repeat = 1
-        self.category = 'person'
+
+def sample(sample_dataset, model, iter, gen_dir):
+    model.start_eval()
+    id_list = [0, 1500, 2500, 3500, 4500, 5500, 6500, 7500, 8500, 10000]
+    for id in id_list:
+        index, bg_feat, fg_feat, fg_msk_feat, fg_bbox, comp_feat, comp_msk_feat, comp_crop_feat, label, trans_label, catnm = sample_dataset[id]
+        pred_img, pred_msk, pred_trans = model.test_genorator(bg_feat.unsqueeze(0), fg_feat.unsqueeze(0), fg_msk_feat.unsqueeze(0), torch.Tensor(fg_bbox).unsqueeze(0))
+        img = transforms.ToPILImage()(pred_img.cpu()[0]).convert('RGB')
+        sample_dir = os.path.join(gen_dir, str(id))
+        if not os.path.exists(sample_dir):
+            os.makedirs(sample_dir)
+        img.save(os.path.join(sample_dir, '{}.jpg'.format(iter)))
+
+
+def sample(sample_dataset, model, iter, gen_dir):
+    model.start_eval()
+    id_list = [0, 1500, 2500, 3500, 4500, 5500, 6500, 7500, 8500, 10000]
+    for id in id_list:
+        index, bg_feat, fg_feat, fg_msk_feat, fg_bbox, comp_feat, comp_msk_feat, comp_crop_feat, label, trans_label, catnm = sample_dataset[id]
+        pred_img, pred_msk, pred_trans = model.test_genorator(bg_feat.unsqueeze(0), fg_feat.unsqueeze(0), fg_msk_feat.unsqueeze(0), torch.Tensor(fg_bbox).unsqueeze(0))
+        img = transforms.ToPILImage()(pred_img.cpu()[0]).convert('RGB')
+        sample_dir = os.path.join(gen_dir, str(id))
+        if not os.path.exists(sample_dir):
+            os.makedirs(sample_dir)
+        img.save(os.path.join(sample_dir, '{}.jpg'.format(iter)))
+
 
 def infer(eval_loader, opt, model=None, repeat=1):
     def csv_title():
@@ -85,7 +92,28 @@ def infer(eval_loader, opt, model=None, repeat=1):
         for line in gen_res:
             f.write(line + '\n')
 
-def info(comp, mask, bbox, scale):
+
+class args:
+    def __init__(self):
+        self.lr = 0.00002
+        self.d_noise = 1024
+        self.d_model = 512
+        self.d_k = 64
+        self.d_v = 64
+        self.n_heads = 8
+        self.len_k = 84
+        self.b1 = 0.5
+        self.b2 = 0.999
+        self.dst = "OPADst1"
+        self.img_size = 256
+        self.expid = 'graconet'
+        self.data_root = "dataset"
+        self.eval_type = "eval"
+        self.epoch = 11
+        self.repeat = 1
+        self.category = 'person'
+
+def information(comp, mask, bbox, scale):
     annID = '1'
     scID = '2'
     bbox = [605, 23, 116, 328]
@@ -94,12 +122,12 @@ def info(comp, mask, bbox, scale):
     catnm = 'person'
     new_img_path = f'composite/{annID}_{scID}_{bbox[0]}_{bbox[1]}_{bbox[2]}_{bbox[3]}_{scale}_{label}.jpg'
     new_msk_path = f'composite/mask_{annID}_{scID}_{bbox[0]}_{bbox[1]}_{bbox[2]}_{bbox[3]}_{scale}_{label}.jpg'
-    composite_path = os.path.join('new_OPA', new_img_path[:-4]+'.png')
-    mask_path = os.path.join('new_OPA', new_msk_path[:-4]+'.png')
+    composite_path = os.path.join('dataset', new_img_path[:-4]+'.png')
+    mask_path = os.path.join('dataset', new_msk_path[:-4]+'.png')
     comp.save(composite_path)
-    os.rename(composite_path, 'new_OPA/'+new_img_path)
+    os.rename(composite_path, 'dataset/'+new_img_path)
     mask.save(mask_path)
-    os.rename(mask_path, 'new_OPA/'+new_msk_path)
+    os.rename(mask_path, 'dataset/'+new_msk_path)
     # annID = '3'
     # scID = '4'
     # bbox = '[216, 100, 363, 546]'
@@ -120,27 +148,44 @@ def info(comp, mask, bbox, scale):
             bbox, scale, int(label), catnm,
             new_img_path, new_msk_path]
     return [info]
-    
-def place(scale=0.99):
+
+def place(foreground='input/foreground.png',background='input/background.png',fg_mask='output_matting/fg_mask.png',scale=0.99):
     opt = args()
     scale = scale
     category = opt.category
+    os.mkdir('dataset')
+    os.mkdir('dataset/background')
+    os.mkdir('dataset/foreground')
+    os.mkdir('dataset/transparent_mask')
+    os.mkdir('dataset/background/person')
+    os.mkdir('dataset/foreground/person')
+    os.mkdir('dataset/transparent_mask/person')
     # path to transparent foreground
-    foreground = f'new_OPA/transparent_mask/{category}/1.png'
+    # foreground = f'dataset/transparent_mask/{category}/1.png'
+    # foreground = f'input/foreground.png'
     # path to background
-    background = f'new_OPA/background/{category}/2.jpg'
+    # background = f'dataset/background/{category}/2.jpg'
+    # background = f'input/background.png'
     # generate composite images and receive mask
     comp, mask, bbox = compose_images(foreground, background)
     # path to mask for foreground
-    foreground_mask = f'new_OPA/transparent_mask/{category}/mask_1.png'
-    # if not os.path.exists(f'new_OPA/foreground/{category}'):
-    #     os.mkdir(f'new_OPA/foreground/{category}')
-    category_dir = f'new_OPA/foreground/{category}/'
-    # shutil.copy(background, f'new_OPA/background/{category}/2.jpg')
-    shutil.copy(foreground, category_dir+'1.jpg')
-    shutil.copy(foreground_mask, category_dir+'mask_1.jpg')
-    os.mkdir('new_OPA/composite/')
-    info = info(comp, mask, bbox, scale)
+    foreground_mask = f'output_matting/fg_mask.png'
+    foreground_dir = f'dataset/foreground/{category}/'
+    # shutil.copy(background, f'dataset/background/{category}/2.jpg')
+    shutil.copy(foreground, foreground_dir+'1.jpg')
+    shutil.copy(background,'dataset/background/person/2.jpg')
+    shutil.copy(foreground_mask, foreground_dir+'mask_1.jpg')
+    if not os.path.exists('dataset/composite/'):
+        os.mkdir('dataset/composite/')
+    info = information(comp, mask, bbox, scale)
     eval_loader = get_loader(opt.dst, batch_size=1, num_workers=1, image_size=opt.img_size, shuffle=False, mode_type=opt.eval_type, data_root=opt.data_root, info=info)
     with torch.no_grad():
         infer(eval_loader, opt, model=None, repeat=opt.repeat)
+    os.mkdir('../output_object_placement')
+    image_name = os.listdir('result/graconet/eval/11/images')[0]
+    mask_name = os.listdir('result/graconet/eval/11/masks')[0]
+    shutil.copy('result/graconet/eval/11/images/'+image_name, '../output_object_placement/composite_image.png')
+    shutil.copy('result/graconet/eval/11/masks/'+mask_name, '../output_object_placement/composite_mask.png')
+
+if __name__ == '__main__':
+  place()
